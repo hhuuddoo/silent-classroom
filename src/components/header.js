494d290fb2e98ef8a3392ab4af29acc8e1a4f3e4
Hudson Ballina <n10219641@qut.edu.au>
2021-04-05 11:50:09 +1000

    finalised mobile home page styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,18 @@ import { ReactComponent as Logo } from "../images/logo.svg";
 
 export default function Header() {
   return (
-    <header className="main-header">
-      <nav className="main-header__nav">
-        <ul className="main-header__nav--items">
-          <li className="main-header__nav--item">
+    <header className="header">
+      <nav className="header__nav">
+        <ul className="header__nav--items">
+          <li className="header__nav--item">
             <a href="">
               <Logo />
             </a>
           </li>
-          <li className="main-header__nav--item">
+          <li className="header__nav--item">
             <a href="">Login</a>
           </li>
-          <li className="main-header__nav--item">
+          <li className="header__nav--item">
             <a href="">Signup</a>
           </li>
         </ul>
