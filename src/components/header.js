bdb72da6356f0c5d0e3e14b32453613134857c40
Hudson Ballina <n10219641@qut.edu.au>
2021-04-05 12:54:08 +1000

    add mobile nav styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { ReactComponent as Logo } from "../images/logo.svg";
 
-export default function Header() {
+export default function Header({ hamburgerIsOpen }) {
   return (
     <header className="header">
       <nav className="header__nav">
         <ul className="header__nav--items">
-          <li className="header__nav--item">
+          <li className="header__nav--item logo">
             <a href="">
               <Logo />
             </a>
@@ -16,6 +16,12 @@ export default function Header() {
           </li>
           <li className="header__nav--item">
             <a href="">Signup</a>
+          </li>
+          <li className="header__nav--item hamburger hamburger--open">
+            <a href="">
+              <span className="hamburger__top"></span>
+              <span className="hamburger__bottom"></span>
+            </a>
           </li>
         </ul>
       </nav>
