4779e176e28cebc86bc8d934fd4188ccf2255e80
Hudson Ballina <n10219641@qut.edu.au>
2021-04-05 18:37:10 +1000

    add question input placeholder

--- src/components/question-input.js
+++ src/components/question-input.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DOMPurify from "dompurify";
 
+// Input for entering questions
 export default function QuestionInput({ question, setQuestion }) {
   // Avoid XSS attacks since im setting innerHTML
   const sanitiser = DOMPurify.sanitize;
@@ -12,7 +13,6 @@ export default function QuestionInput({ question, setQuestion }) {
       contentEditable={true}
       dangerouslySetInnerHTML={{ __html: sanitiser(question) }}
       onChange={({ target }) => setQuestion(target.value)}
-      aria-description="Question Textarea"
     ></div>
   );
 }
