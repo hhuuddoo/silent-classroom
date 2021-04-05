5152565affa67ae3f8361966b3bc2b097b23c058
Hudson Ballina <n10219641@qut.edu.au>
2021-04-05 16:04:11 +1000

    add mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,24 @@ import { ReactComponent as Logo } from "../images/logo.svg";
 export default function Header(props) {
   const { hamburgerIsOpen, setHamburgerIsOpen } = props;
 
+  const toggleHamburger = () => {
+    setHamburgerIsOpen((prev) => !prev);
+  };
+
   return (
     <header className="header">
+      <div
+        className={`overlay hide-on-desktop ${
+          hamburgerIsOpen ? `hamburger--open` : ``
+        }`}
+      ></div>
       <nav className="container">
         <a className="header__logo" href="">
           <Logo />
         </a>
         <a
           href="#"
-          onClick={() => setHamburgerIsOpen((prev) => !prev)}
+          onClick={toggleHamburger}
           className={`header__hamburger hide-on-desktop ${
             hamburgerIsOpen ? `header__hamburger--open` : ``
           }`}
@@ -32,7 +41,11 @@ export default function Header(props) {
             Create Board
           </a>
         </div>
-        <div className="header__menu hide-on-desktop">
+        <div
+          className={`header__menu hide-on-desktop ${
+            hamburgerIsOpen ? `hamburger--open` : ``
+          }`}
+        >
           <a className="button button--cta" href="">
             Create Board
           </a>
