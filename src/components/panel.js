d4a4b6df411e372c93734c930d5beefe24f831cb
Hudson Ballina <n10219641@qut.edu.au>
2021-04-05 18:43:45 +1000

    fix panel margins

--- src/components/panel.js
+++ src/components/panel.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Panel({ children }) {
-  return <div className="panel">{children}</div>;
+  return (
+    <div className="panel-container">
+      <div className="panel">{children}</div>
+    </div>
+  );
 }
