552d5787c146358caf01cf559a3325aa79abbaf4
Hudson Ballina <n10219641@qut.edu.au>
2021-04-07 09:22:45 +1000

    add vote button

--- src/containers/poll.js
+++ src/containers/poll.js
@@ -10,6 +10,9 @@ export default function Poll({ question, options }) {
       <PollOption option="Option 2" name="some id" id="id2" />
       <PollOption option="Option 3" name="some id" id="id3" />
       <Seperator />
+      <div className="right">
+        <span className="button button--cta">Vote</span>
+      </div>
     </Panel>
   );
 }
