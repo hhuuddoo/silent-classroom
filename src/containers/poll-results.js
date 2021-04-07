4373d6afe78a95676ff8e003bfcad4e9f90b04b1
Hudson Ballina <n10219641@qut.edu.au>
2021-04-07 11:10:28 +1000

    fixed 0 percent result bug

--- src/containers/poll-results.js
+++ src/containers/poll-results.js
@@ -7,8 +7,8 @@ export default function PollResults({ question }) {
       <Question>{question}</Question>
       <Seperator />
       <PollOptionResult option="0" percentage="30" votes="3" />
-      <PollOptionResult option="1" percentage="60" votes="6" />
-      <PollOptionResult option="10" percentage="10" votes="1" />
+      <PollOptionResult option="1" percentage="0" votes="6" />
+      <PollOptionResult option="10" percentage="100" votes="10" />
     </Panel>
   );
 }
