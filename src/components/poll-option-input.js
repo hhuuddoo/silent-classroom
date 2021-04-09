d058cdd02e74b5ae0adc3389fe0be35999767fd2
Hudson Ballina <n10219641@qut.edu.au>
2021-04-09 18:39:13 +1000

    add invalid argument to poll option component

--- src/components/poll-option-input.js
+++ src/components/poll-option-input.js
@@ -2,11 +2,11 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 
 // Poll Option Input Text Box
-const PollOptionInput = observer(({ store, pollId, optionId }) => {
+const PollOptionInput = observer(({ store, pollId, optionId, invalid }) => {
   return (
     <>
       <input
-        className={`input input--option`}
+        className={`input input--option ${invalid ? `invalid` : ``}`}
         value={store.polls[pollId].options[optionId].option}
         placeholder="Enter poll option..."
         type="text"
